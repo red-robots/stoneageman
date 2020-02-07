29f670e96012515d99ecea68231c05adcff37788
Lisa <lisaqdebona@gmail.com>
2020-02-06 22:02:27 -0700

    Added theme files

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site cf">
+	<a class="skip-link sr" href="#content"><?php esc_html_e( 'Skip to content', 'bellaworks' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="wrapper">
+			
+			<?php if( get_custom_logo() ) { ?>
+	            <div class="logo">
+	            	<?php the_custom_logo(); ?>
+	            </div>
+	        <?php } else { ?>
+	            <h1 class="logo">
+		            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+	            </h1>
+	        <?php } ?>
+
+
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'MENU', 'bellaworks' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div><!-- wrapper -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content wrapper">
