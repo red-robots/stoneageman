f1528a980d93794c2d28f864c2f8e03d2ac9694f
Austin Crane <austincrane@gmail.com>
2020-02-27 14:09:22 -0500

    add analytics

--- footer.php
+++ footer.php
@@ -32,6 +32,14 @@
 </div><!-- #page -->
 <script src="https://www.youtube.com/iframe_api"></script>
 <?php wp_footer(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-159268763-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-159268763-1');
+</script>
 </body>
 </html>
